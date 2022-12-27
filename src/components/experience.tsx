380b2a74b70262e5ae84360539d6f59c87a31c8c
Jack Frate <jack.frate24@gmail.com>
2022-12-27 01:02:30 -0500

    finish basic experience layout

--- src/components/experience.tsx
+++ src/components/experience.tsx
@@ -33,19 +33,20 @@ const jobItems: JobItem[] = [
 ];
 
 const Experience = () => {
-
   return (
     <div className="hero h-full w-screen grow bg-base-100">
-      <div className="hero-content relative flex h-full w-full flex-col lg:gap-16">
-        {/* job desc */}
-        <div className="space-between relative flex w-full flex-col">
+      <div className="hero-content relative flex h-full w-full flex-col overflow-y-scroll lg:gap-16">
+        <div className="space-between relative flex w-full  flex-col gap-8 px-11">
           {jobItems.map((jobItem) => (
             <>
-              <div className="flex min-w-full flex-row items-center justify-between">
-                <h1 className="text-3xl font-bold">{jobItem.jobTitle}</h1>
-                <p>
-                  {jobItem.startDate} - {jobItem.endDate}
-                </p>
+              <div className="flex min-w-full flex-row items-end justify-between">
+                <h1 className="text-3xl font-bold">{jobItem.companyName}</h1>
+                <p className="text-2xl">{jobItem.jobTitle}</p>
+                <div className="flex flex-col">
+                  <p>
+                    {jobItem.startDate} - {jobItem.endDate}
+                  </p>
+                </div>
               </div>
               <ul className="flex flex-col">
                 {jobItem.jobPoints.map((point) => (
