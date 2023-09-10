e7ce5ae333b5e813161caa951afede2ca95f971a
Jack Frate <jack.frate24@gmail.com>
2023-09-10 13:28:29 -0500

    small clean up

--- src/components/train-tracker/train-listing/TrainListing.tsx
+++ src/components/train-tracker/train-listing/TrainListing.tsx
@@ -7,7 +7,6 @@ type TrainListingProps = {
   secondsSineLastUpdate: number;
 };
 
-// function that converts a date to hh:MM:ss format
 function padTo2Digits(num: number) {
   return num.toString().padStart(2, "0");
 }
@@ -20,25 +19,14 @@ function convertMsToTime(milliseconds: number) {
   seconds = seconds % 60;
   minutes = minutes % 60;
 
-  // 👇️ If you want to roll hours over, e.g. 00 to 24
-  // 👇️ uncomment the line below
-  // uncommenting next line gets you `00:00:00` instead of `24:00:00`
-  // or `12:15:31` instead of `36:15:31`, etc.
-  // 👇️ (roll hours over)
-  // hours = hours % 24;
-
   return `${padTo2Digits(hours)}:${padTo2Digits(minutes)}:${padTo2Digits(
     seconds
   )}`;
 }
 
-const roundTrainEtaTime = (timeInMs: number) => {
-  return Math.round(timeInMs / 1000) * 1000;
-};
-
 const getTimeUntilArrival = (trainEta: TrainEta, msSinceLastUpdate: number) => {
   const currentTimeMs = Date.now();
-  const arrivalTimeMs = roundTrainEtaTime(Date.parse(trainEta.arrivalTime));
+  const arrivalTimeMs = Date.parse(trainEta.arrivalTime);
 
   const timeUntilArrivalMs = arrivalTimeMs - currentTimeMs;
 
